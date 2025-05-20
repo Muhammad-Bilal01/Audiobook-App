ae24ffe74d4aaf0a42b20ba2c25b341e6a64e1b7
Muhammad Bilal <mbilal254.m@gmail.com>
2025-05-21 03:13:30 +0500

    add tab icons

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,5 +1,18 @@
 import { Tabs } from "expo-router";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 export default function TasbLayout() {
-    return <Tabs />;
+    return <Tabs screenOptions={{
+        tabBarShowLabel: false,
+    }}>
+        <Tabs.Screen name="index" options={{
+            title: "Library",
+            tabBarIcon: ({ size, color }) => <Ionicons name="library" size={size} color={color} />
+
+        }} />
+        <Tabs.Screen name="discover" options={{
+            title: "Discover",
+            tabBarIcon: ({ size, color }) => <Ionicons name="search" size={size} color={color} />
+        }} />
+    </Tabs>;
 }
